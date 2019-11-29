2d59a38a5f97df2f6dff53546becbf1f9a6a7c15
kimsean <kshonne.developer@gmail.com>
2019-11-29 21:48:52 +0800

    added scss

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default class HomeComponent extends React.Component {
   constructor () {
     super()
+    this.state = {
+      
+    }
   }
   render() {
     return (
